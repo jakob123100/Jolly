0c3e4dfca257e83d425430aeb76242ab420a817f
Jakob Söderström <jakso277@student.liu.se>
2023-12-23 15:55:47 +0100

    hardware test sleep added

--- hardware_test.py
+++ hardware_test.py
@@ -1,5 +1,6 @@
 from servo_controller import servo_controller
 from led_controller import led_controller, colors
+from time import sleep
 
 def test():
     sc = servo_controller()
@@ -8,19 +9,31 @@ def test():
     sc.move_right_arm(90)
     sc.move_left_arm(90)
     sc.move_head(90)
+
+    sleep(1)
+
     lc.set_eye_color(colors.green)
     sc.move_right_arm(0)
     sc.move_left_arm(0)
     sc.move_head(0)
+
+    sleep(1)
+
     lc.set_eye_color(colors.blue)
     sc.move_right_arm(180)
     sc.move_left_arm(180)
     sc.move_head(180)
+
+    sleep(1)
+
     lc.set_eye_color(colors.black)
     sc.move_right_arm(90)
     sc.move_left_arm(90)
     sc.move_head(90)
     lc.set_light_string(True)
+
+    sleep(1)
+
     lc.set_light_string(False)
 
 if __name__ == '__main__':
