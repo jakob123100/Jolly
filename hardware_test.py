4bb363f460f1078e9b46871bd3a5856bb28a430b
Jakob Söderström <jakso277@student.liu.se>
2023-12-23 15:54:44 +0100

    hardware test adde

--- hardware_test.py
+++ hardware_test.py
@@ -1,22 +1,22 @@
-import servo_controller
-import led_controller
+from servo_controller import servo_controller
+from led_controller import led_controller, colors
 
 def test():
-    sc = servo_controller.servo_controller()
-    lc = led_controller.led_controller()
-    lc.set_eye_color(lc.colors.red)
+    sc = servo_controller()
+    lc = led_controller()
+    lc.set_eye_color(colors.red)
     sc.move_right_arm(90)
     sc.move_left_arm(90)
     sc.move_head(90)
-    lc.set_eye_color(lc.colors.green)
+    lc.set_eye_color(colors.green)
     sc.move_right_arm(0)
     sc.move_left_arm(0)
     sc.move_head(0)
-    lc.set_eye_color(lc.colors.blue)
+    lc.set_eye_color(colors.blue)
     sc.move_right_arm(180)
     sc.move_left_arm(180)
     sc.move_head(180)
-    lc.set_eye_color(lc.colors.off)
+    lc.set_eye_color(colors.black)
     sc.move_right_arm(90)
     sc.move_left_arm(90)
     sc.move_head(90)
